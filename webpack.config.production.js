3bafc70226da9c7257ac36c68e98f81d5c5ea2d3
SamuelTribeUK <SamuelTribeUK@outlook.com>
2021-02-08 19:03:32 +0000

    Refactored webpack configs to use merging to reduce repeated code

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,30 +1,6 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.config.js');
 
-module.exports = {
-	mode: "production",
-	entry: {
-		app: ['./src/index.js','./src/settingsPanel.js'],
-	},
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist'),
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/i,
-				use: ['style-loader', 'css-loader'],
-			},
-		],
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			title: '3D Game of Life - Samuel Tribe',
-			template: path.resolve(__dirname, './src/template.html'),
-			filename: 'index.html'
-		}),
-		new CleanWebpackPlugin(),
-	],
-};
+module.exports = merge(common, {
+	mode: "production"
+});
