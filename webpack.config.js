474f80d7b2e2b66f4d20e7784f8ed42691791423
SamuelTribeUK <60364181+SamuelTribeUK@users.noreply.github.com>
2021-02-24 11:32:10 +0000

    Update webpack.config.js
    Removed the "import-export" from the version string ready for production

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
 	plugins: [
 		new HtmlWebpackPlugin({
 			title: '3D Game of Life - Samuel Tribe',
-			version: '1.1 import-export',
+			version: '1.1',
 			template: path.resolve(__dirname, './src/template.html'),
 			filename: 'index.html',
 			favicon: 'src/assets/images/favicon.ico'
