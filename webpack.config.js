7fdaae4d086e411ab459c577de22686357e90bbc
SamuelTribeUK <60364181+SamuelTribeUK@users.noreply.github.com>
2021-02-24 14:00:00 +0000

    Update webpack.config.js
    Updated version number (1.1 for now, the next full production build on master will be 2.0)

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
 	plugins: [
 		new HtmlWebpackPlugin({
 			title: '3D Game of Life - Samuel Tribe',
-			version: '1.1 bug-font-load',
+			version: '1.1',
 			template: path.resolve(__dirname, './src/template.html'),
 			filename: 'index.html',
 			favicon: 'src/assets/images/favicon.ico'
