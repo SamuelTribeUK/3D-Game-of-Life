408bbe2b4436ba5f35daa15e39cef05d333baf6d
SamuelTribeUK <SamuelTribeUK@outlook.com>
2021-03-12 17:19:10 +0000

    Included jsdoc comment for notify function

--- src/notification.js
+++ src/notification.js
@@ -1,5 +1,12 @@
 import Toastify from "toastify-js";
 
+/**
+ * The notify function handles toastify-js notifications with custom text, colour and duration.
+ * @param {string} text - The text to be displayed in the notification
+ * @param {string} type - The type of notification (either success or error). This dictates the colour of the
+ * notification, with "success" having a green gradient and "error" having an orange gradient
+ * @param {number} duration - The duration in milliseconds that the notification will be displayed for
+ */
 export function notify(text, type, duration) {
 	let backgroundColor;
 	if (type === "success") {
